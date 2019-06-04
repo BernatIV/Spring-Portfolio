340d2f4a61151ab7b3fe6ae6730513ad0fc792a1
Bernat Quart <marc.mane.duatis@gmail.com>
2019-06-04 20:30:00 +0200

    Update Zuul for authentication

--- SpringEurekaAuth/src/test/java/com/eureka/auth/SpringEurekaAuth/SpringEurekaAuthApplicationTests.java
+++ SpringEurekaAuth/src/test/java/com/eureka/auth/SpringEurekaAuth/SpringEurekaAuthApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eureka.auth.tests;
+package com.eureka.auth.SpringEurekaAuth;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringEurekaServiceApplicationTests {
+public class SpringEurekaAuthApplicationTests {
 
 	@Test
 	public void contextLoads() {
